3562dac396d3db74f7f2659aba2ca24be5f8d19e
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-04 05:01:27 +0100

    wedas

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+import {Inter} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { headers } from "next/headers";
+import contextProvider from "@/context/index";
 
 export const metadata = {
   title: "Create Next App",
@@ -17,12 +13,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const cookies = headers().get("cookie");
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <contextProvider cookies={cookies}>{children}</contextProvider>
       </body>
     </html>
   );
