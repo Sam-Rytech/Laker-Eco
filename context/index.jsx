d7cc3ffe96a86ddf45ce94caccca3eb656ccff93
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-10-04 04:44:27 +0100

    jeo

--- context/index.jsx
+++ context/index.jsx
@@ -1,8 +1,9 @@
 "use client";
 import { wagmiAdapter, projectId } from "@/config";
 import { createAppKit } from "@reown/appkit";
-import { mainnet, base } from "@reown/appkit/networks";
+import { mainnet, base, ancient8 } from "@reown/appkit/networks";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { adapter } from "next/dist/server/web/adapter";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, typeConfig } from "wagmi";
 
@@ -11,4 +12,19 @@ if(!projectId){
   throw new Error("Project ID is not defined");
 }
 
-cons
+const metadata = {
+    name: "Laker Eco",
+    description: "An ecosytem for Lakers",
+    url: "https://laker-eco.vercel.app",
+    icons: ["https://laker-eco.vercel.app/favicon.ico"]
+};
+const modal: createAppKit({
+    adapter: [wagmiAdapter], projectId, networks: [mainnet, base], defaultNetwork: mainnet, features: {
+        analytics: true,
+        email: true,
+        socials: ["google", "github", "discord"],
+        emailShowWallet: true,
+    },
+    themeMode: "light",
+})
+
